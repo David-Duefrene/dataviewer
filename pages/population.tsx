d118cda1f45c29735b80db00179e3f0c613d8828
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2023-02-06 16:41:43 -0700

    Population now pulls from atlas

--- pages/population.tsx
+++ pages/population.tsx
@@ -1,11 +1,9 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 
 import { useState } from 'react'
 
-import data from './data.json';
-import ChartBox from '../Components/ChartBox/ChartBox';
+import ChartBox from '../Components/ChartBox/ChartBox'
+import clientPromise from '../util/mongoClient'
 import styles from '../styles/population.module.scss'
 
 interface ChartData {
@@ -13,9 +11,37 @@ interface ChartData {
 	value: number;
 }
 
-const Home: NextPage = () => {
+export const getServerSideProps = async () => {
+    try {
+        const client = await clientPromise
+        const db = client.db('DataViewer')
+
+        const dataSet = await db
+            .collection('population')
+            .find({})
+            .toArray()
+        const json =  JSON.parse(JSON.stringify(dataSet))[0]
+        delete json._id
+
+
+        return {
+            props: { countyJSON: json },
+        }
+    } catch (e) {
+        console.error(e)
+        throw new Error('Failed to connect to database')
+    }
+}
+
+interface HomeProps {
+    countyJSON: Record<string, ChartData[]>;
+}
+
+
+const Home = ({ countyJSON }: HomeProps) => {
 	const [county, setCounty] = useState(['Denver', 'El Paso']);
-	const countyList = Object.keys(data).map(entry => {
+
+	const countyList = Object.keys(countyJSON).map(entry => {
 		return (
 			<li key={entry}>
 				<button
@@ -35,12 +61,13 @@ const Home: NextPage = () => {
 			</li>
 		);
 	});
-	const dataSets: any = [];
 
-	county.forEach(entry => {
+    const dataSets: any = [];
+
+    county.forEach(entry => {
 		const newData = [];
         // @ts-ignore
-        for (const dataPoint of data[entry]) {
+        for (const dataPoint of countyJSON[entry]) {
 			newData.push({
 				date: dataPoint.year,
 				value: dataPoint.totalPopulation
@@ -50,22 +77,22 @@ const Home: NextPage = () => {
 		dataSets.push({data: newData, name: entry});
 	});
 
-  return (
-    <>
-      <Head>
-        <title>Population Chart</title>
-        <meta name="Population chart for Colorado" content="generated from data.colorado.gov" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <>
+            <Head>
+                <title>Population Chart</title>
+                <meta name="Population chart for Colorado" content="generated from data.colorado.gov" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <main className={styles.App}>
-	  	<aside className={styles.SidePanel}>
-			<ul className={styles.SelectionList}>{countyList}</ul>
-		</aside>
-		<ChartBox data={dataSets} />
-      </main>
-    </>
-  )
+            <main className={styles.App}>
+                <aside className={styles.SidePanel}>
+                    <ul className={styles.SelectionList}>{countyList}</ul>
+                </aside>
+                <ChartBox data={dataSets} />
+            </main>
+        </>
+    )
 }
 
 export default Home
