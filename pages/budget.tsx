16b45629af807509e8ab3276ebc9085a896d4aed
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2023-02-08 16:40:39 -0700

    budget now pull from mongo

--- pages/budget.tsx
+++ pages/budget.tsx
@@ -3,11 +3,35 @@ import Head from 'next/head'
 import { useState } from 'react'
 
 import ChartBox from '../Components/ChartBox/ChartBox'
-// Import clientPromise from '../util/mongoClient'
-import data from './budget.json'
+import clientPromise from '../util/mongoClient'
 import styles from '../styles/population.module.scss'
 
-const Budget = () => {
+export const getServerSideProps = async () => {
+	try {
+		const client = await clientPromise
+		const db = client.db('DataViewer')
+
+		const dataSet = await db
+			.collection('Budget')
+			.find({ year: 2021 })
+			.toArray()
+		const json = JSON.parse(JSON.stringify(dataSet))[0]
+		delete json._id
+
+		return {
+			props: { data: json },
+		}
+	} catch (e) {
+		console.error(e)
+		throw new Error('Failed to connect to database')
+	}
+}
+
+type Props = {
+	data: Record<string, any>;
+}
+
+const Budget = ({ data }: Props) => {
 	const [ selection, setSelection ] = useState('cabinet_list')
 	const [ subSelection, setSubSelection ] = useState('total')
 	const [ dataSet, setDataSet ] = useState(data[selection])
