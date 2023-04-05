3e750f0f3982d15c599cddd33f649ccb0ca5fd6f
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2023-04-05 11:49:18 -0600

    33 move line color selection into separate generator function (#34)
    Created a color key in the Chart Box component that hides behind the
    Control Panel component

--- Components/ChartBox/ChartBox.tsx
+++ Components/ChartBox/ChartBox.tsx
@@ -1,38 +1,23 @@
 import React, { useState, useEffect } from 'react'
 
-import LineChart from './LineChart/LineChart'
 import * as d3 from 'd3'
 
-export interface ChartData {
-	date: string;
-	value: number;
-}
-
-interface ChartBoxData {
-	data: ChartData[];
-	name: string;
-}
+import LineChart from './LineChart'
+import { ChartBoxData, ChartData } from './types'
+import styles from './ChartBox.module.sass'
 
 interface ChartBoxProps {
 	data: ChartBoxData[],
     title: string
 }
 
-/**
- *  ChartBox is a React wrapper for LineChart.
- */
 const ChartBox = ({ data, title }: ChartBoxProps) => {
 	const svgRef = React.useRef(null)
 
-	/**
-	 * The width of the chart
-	 */
 	const [ width, setWidth ] = useState(1500)
-
-	/**
-	 * The height of the chart
-	 */
 	const [ height, setHeight ] = useState(750)
+
+	const linesColors: React.ReactElement<unknown, string> | JSX.Element[] = []
 
 	// Resize the chart on window resize
 	useEffect(() => {
@@ -44,31 +29,48 @@ const ChartBox = ({ data, title }: ChartBoxProps) => {
 		return () => window.removeEventListener('resize', handleResize)
 	}, [])
 
-	useEffect(() => {
-		let min = Number.MAX_SAFE_INTEGER
-		let max = 0
+	let min = Number.MAX_SAFE_INTEGER
+	let max = 0
 
-		for (const county of data) {
-			const testMin = d3.min(county.data, (d: ChartData) => d.value) || Number.MAX_SAFE_INTEGER
-			const testMax = d3.max(county.data, (d: ChartData) => d.value) || max
+	data.map((dataPoint: ChartBoxData, index: number) => {
+		const testMin = d3.min(dataPoint.data, (d: ChartData) => d.value) || Number.MAX_SAFE_INTEGER
+		const testMax = d3.max(dataPoint.data, (d: ChartData) => d.value) || max
 
-			if (testMin < min) {
-				min = testMin
-			}
-			if (testMax > max) {
-				max = testMax
-			}
+		if (testMin < min) {
+			min = testMin
 		}
-		const dimensions = { width, height }
+		if (testMax > max) {
+			max = testMax
+		}
 
+		const color = `var(--line-color-${index%10})`
+		const name = dataPoint.name
+		dataPoint.lineColor = color
+		linesColors.push(
+			<tr className={styles.Row} key={`liceColor-${name}`}>
+				<td>{name}</td>
+				<td className={styles.Color} style={{ backgroundColor: color }} />
+			</tr>,
+		)
+	})
+
+	useEffect(() => {
+		const dimensions = { width, height }
 		LineChart({
 			data, dimensions, svgRef, min, max,
 		})
-	}, [ data, height, width, svgRef ])
+	}, [ data, width, height, svgRef, min, max ])
 
 	return (
 		<article>
 			<h1>{title}</h1>
+			<table className={styles.ColorKey}>
+				<tr>
+					<th>Name</th>
+					<th>Color</th>
+				</tr>
+				{linesColors}
+			</table>
 			<svg className='Chart' ref={svgRef} width={width + 120} height={height * 1.1} />
 		</article>
 	)
---
+++ Components/ChartBox/types.ts
@@ -0,0 +1,10 @@
+export interface ChartData {
+	date: string
+	value: number
+}
+
+export interface ChartBoxData {
+	data: ChartData[]
+	name: string
+	lineColor: string
+}
