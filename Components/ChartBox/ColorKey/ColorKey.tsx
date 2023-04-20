771c0f413c7e5827989e4296bba97717f1943eff
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2023-04-19 22:42:02 -0600

    55 collapse color key (#58)

--- Components/ChartBox/ColorKey/ColorKey.tsx
+++ Components/ChartBox/ColorKey/ColorKey.tsx
@@ -1,24 +1,19 @@
 import styles from './ColorKey.module.sass'
 
 const ColorKey = ({ children }: { children: JSX.Element[] | JSX.Element }) => {
-	return <table className={styles.ColorKey}>
-		<tbody>
-			<tr>
-				<td>Name</td>
-				<td>Color</td>
-			</tr>
+	return (
+		<div className={styles.ColorKey}>
 			{children}
-		</tbody>
-	</table>
+		</div>
+	)
 }
 
 export default ColorKey
 
 export const ColorRow = ({ name, color }: { name: string, color: string }) => {
 	return (
-		<tr className={styles.Row} key={`lineColor-${name}`}>
-			<td>{name}</td>
-			<td className={styles.Color} style={{ backgroundColor: color }} />
-		</tr>
+		<div className={styles.Color} style={{ backgroundColor: color }} key={`lineColor-${name}`}>
+			{name}
+		</div>
 	)
 }
