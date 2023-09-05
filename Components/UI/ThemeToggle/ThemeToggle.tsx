c3a0a71dc2759791a4631b516d0227f4d29ce873
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2023-09-05 11:29:39 -0600

    Fixed light mode issue
    Light mode now correct SSR.

--- Components/UI/ThemeToggle/ThemeToggle.tsx
+++ Components/UI/ThemeToggle/ThemeToggle.tsx
@@ -3,13 +3,14 @@ import { useState, useEffect } from 'react'
 import styles from './ThemeToggle.module.sass'
 
 const ThemeToggle = () => {
-	// Is Next.js SSR compatible here? I don't think so
-	const isDarkMode: boolean = window.matchMedia('(prefers-color-scheme: dark)').matches
+	const isDarkMode: boolean = false
 	const [ theme, setTheme ] = useState<'Dark' | 'Light'>(isDarkMode ? 'Dark' : 'Light')
 
 	useEffect(() => {
 		const updateTheme = () => setTheme(isDarkMode ? 'Dark' : 'Light')
 		const matchMedia = window.matchMedia('(prefers-color-scheme: dark)')
+
+		if (matchMedia.matches) setTheme('Dark')
 		matchMedia.addEventListener('change', updateTheme)
 
 		return () => matchMedia.removeEventListener('change', updateTheme)
@@ -25,7 +26,7 @@ const ThemeToggle = () => {
 		setTheme(theme === 'Dark' ? 'Light' : 'Dark')
 	}
 
-	return <button className={styles.Button} onClick={clickable}>{theme === 'Dark' ? 'Light' : 'Dark' }</button>
+	return <button className={styles.Button} onClick={clickable}>{theme}</button>
 }
 
 export default ThemeToggle
